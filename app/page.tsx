f1547ae2d78365bf6083a49add8f2d886a48e7ef
Shrey(personal) <shreypersonal@shreyashs-Laptop.local>
2024-03-22 12:12:12 +0530

    as

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="p-20">
       <div className="p-8 m-8"> </div>
       <div className="main text-5xl text-white">
-        <p><span className="mukta-regular">I&apos;M </span><span className="name-shreyash text-7xl font-semibold arbutus-slab-regular pl-1">Shreyash Wadmalwar</span></p>
+        <p><span className="mukta-regular">I&apos;M </span><span className="name-shreyash text-7xl font-semibold arbutus-slab-regular pl-1">Shreyash Wadmalwarss</span></p>
         <br />
         <p className="mukta-regular">PHP & Web Developer</p>
       </div>
